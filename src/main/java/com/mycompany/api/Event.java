6412d3647e27cc070e39cec2dc7ef1c2572cfd97
Indrek Ots <ots.indrek@gmail.com>
2016-10-03 11:48:30 +0200

    Create a representation for Events

---
+++ src/main/java/com/mycompany/api/Event.java
@@ -0,0 +1,51 @@
+package com.mycompany.api;
+
+import java.util.Date;
+
+public class Event {
+    private long id;
+    private String name;
+    private String description;
+    private String location;
+    private Date date;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}

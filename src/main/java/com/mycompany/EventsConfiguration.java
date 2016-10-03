0fc1d5d590caa716d5475b79d11a127ef334fda1
Indrek Ots <ots.indrek@gmail.com>
2016-10-03 11:38:10 +0200

    Add configuration fields

--- src/main/java/com/mycompany/EventsConfiguration.java
+++ src/main/java/com/mycompany/EventsConfiguration.java
@@ -6,5 +6,11 @@ import org.hibernate.validator.constraints.*;
 import javax.validation.constraints.*;
 
 public class EventsConfiguration extends Configuration {
-    // TODO: implement service configuration
+
+    @NotEmpty
+    private String dateFormat;
+
+    public String getDateFormat() {
+        return dateFormat;
+    }
 }

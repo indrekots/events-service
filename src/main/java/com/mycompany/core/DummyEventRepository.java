ec58b9140dc82e5ab008a038fba0f3b2cda81c04
Indrek Ots <ots.indrek@gmail.com>
2016-12-01 20:33:06 +0200

    Create repository interface and its implementation - DummyEventRepository

---
+++ src/main/java/com/mycompany/core/DummyEventRepository.java
@@ -0,0 +1,38 @@
+package com.mycompany.core;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.type.CollectionType;
+import com.google.common.base.Charsets;
+import com.google.common.io.Resources;
+import com.mycompany.api.Event;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.List;
+
+public class DummyEventRepository implements EventRepository {
+
+    private static final String DATA_SOURCE = "dummy_data.json";
+
+    private List<Event> events;
+
+    public DummyEventRepository() {
+        try {
+            initData();
+        } catch (IOException e) {
+            throw new RuntimeException(DATA_SOURCE +
+                    " is missing or is unreadable");
+        }
+    }
+
+    private void initData() throws IOException {
+        URL url = Resources.getResource(DATA_SOURCE);
+        String json = Resources.toString(url, Charsets.UTF_8);
+        ObjectMapper mapper = new ObjectMapper();
+        CollectionType type = mapper.getTypeFactory()
+                .constructCollectionType(List.class, Event.class);
+        events = mapper.readValue(json, type);
+    }
+
+}
+
---
+++ src/main/java/com/mycompany/core/EventRepository.java
@@ -0,0 +1,5 @@
+package com.mycompany.core;
+
+public interface EventRepository {
+
+}
